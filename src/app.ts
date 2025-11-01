42892ddd8f3a6d7b609c78565950041de138994d
Gabriel Camurça <projetosdocamurca@gmail.com>
2025-11-01 12:15:45 -0300

    relationships migration

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,3 @@
 import fastify from "fastify";
-import { PrismaClient } from "./generated/prisma/index.js";
-
-const prisma = new PrismaClient();
-
-prisma.user.create({
-    data: {
-        name: "Gabriel",
-        email: "teste@teste.com"
-    }
-})
 
 export const app = fastify();
